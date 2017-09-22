435c9add9f9d6f6a250ed337fe27f5d611213704
MLuminary <418037025@qq.com>
2017-09-22 12:20:34 +0800

    add video

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@
       joinBtn = $('.nav-join-link'),
       backTop = $('.nav-join .back');
 
+
   // 根字体自适应
   var bodyWidth = 1980,
       fontSize = 100;
@@ -101,5 +102,43 @@
     }); 
     container.fadeIn();
   },imgList)
+
+  //接受视频地址
+
+  // 与视频有关元素
+
+  var videoBox = $('#video'),
+      videoContent = $('#video video');
+  $.ajax({
+    type : 'get',
+    url : "http://www.kilingzhang.com/api/url.php?url_id=f05485j0nx5",
+    dataType : "text",
+    success : function(resultUrl) {
+      // console.log(resultUrl);
+      videoContent.attr("src",resultUrl);
+    },
+    error : function() {
+      $('#video .msg').fadeIn();
+    }
+  })
+  //点击视频全部控制暂停
+  videoContent[0].onclick = function(){
+    if(this.paused){
+      this.play();
+    }else{
+      this.pause();
+    }
+  }
+
+  //关闭视频
+  $('.close').click(function(){
+    videoBox.fadeOut();
+    videoContent[0].pause();
+  })
+
+  $('.video').click(function(){
+    videoBox.fadeIn();
+    videoContent[0].play();
+  })
 
 })()
